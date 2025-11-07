b9d724a3f75b9cd03ec6143ba1a831851c40d13e
Mohammad Maali <mbmaali474@gmail.com>
2025-11-07 11:19:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+# import selenium
+
+
+mygju_url = "https://mygju.gju.edu.jo/faces/index.xhtml"
+
+def check_website_status():
+    # global mygju_url
+    try:
+        response = requests.get(mygju_url, timeout=5)
+        if response.status_code ==200:
+            return f"Website is working!"
+        else:
+            return f"Website returned error {response.status_code}"
+    except requests.exceptions.RequestException as e:
+        return f"Website is NOT working ❌ ({e})"
+    
+
+
+
+print(check_website_status())
